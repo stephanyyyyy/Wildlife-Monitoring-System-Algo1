b5fb359d151a40c54f6e22a33fd435e797266503
stephanyyyyy <stephany0614@csu.fullerton.edu>
2024-10-13 19:34:23 -0700

    Add files via upload

---
+++ main1.cpp
@@ -0,0 +1,99 @@
+/*Project 1
+    Name: Stephany Murillo Munoz and Andrewdai Truong
+    Algorithm 1: Wildlife Monitoring System
+    Class: CPSC-335-06
+-------------------------------------------------------*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// structure for a coordinate
+struct Coordinate
+{
+	int stepNum; // movement number starting at 0
+	int loc; // animal's location 
+};
+
+
+// function prototypes
+void traverseLocations(vector<int> location);
+
+// main
+int main()
+{
+	int 					numCoord; 		// number of coordiantes
+	vector<Coordinate>		coordinates; 	// a vector of the coordinates
+	Coordinate 				node;	
+
+	// promput user for number of coordinates
+	cout << "Enter number of recorded coordinates: ";
+	cin >> numCoord;
+
+	// promput user for the  coordinates
+	cout << "Enter the coordinates: ";
+	for (int i = 0; i < numCoord; ++i)
+	{
+		cin >> node.stepNum;
+		cin >> node.loc;
+		coordinates.push_back(node);
+	}
+
+	// create vector of ints to hold location values
+	vector<int>				location(numCoord);
+	for (int i = 0; i < numCoord; ++i)
+	{
+		location[i] = coordinates[i].loc;
+	}
+
+	cout << "These are the locations: ";
+	for (int i = 0; i < numCoord ; ++i)
+	{
+		cout << location[i] << " ";
+	}
+
+	cout << endl;
+
+	// display coordinates entered 
+	cout << "Coordinates entered: [";
+	for (size_t i = 0; i < coordinates.size(); ++i)
+	{
+		if (i == coordinates.size() - 1)
+		{
+			cout << "(" << coordinates[i].stepNum << ", " << coordinates[i].loc << ")";
+		}
+		else
+		{
+			cout << "(" << coordinates[i].stepNum << ", " << coordinates[i].loc << "), ";
+		}
+	}
+	cout << "]" << endl << endl;
+
+	traverseLocations(location);
+	
+	return 0;
+}
+
+void traverseLocations(vector<int> loc)
+{
+	bool cycleDetected = false;
+
+	// traverse through locations until a cycle is detected
+	for (unsigned int i = 0; i < loc.size(); ++i)
+	{
+		for (unsigned int j = i + 1; j < loc.size(); ++j)
+		{
+			if (loc[i] == loc[j])
+			{
+				cout << "Loop detected" << endl;
+				cycleDetected = true;
+				break;
+			}
+		}
+	}
+
+	if (cycleDetected == false)
+	{
+		cout << "No loop detected" << endl;
+	}
+}
